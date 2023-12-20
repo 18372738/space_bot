fbace0706826bf1da2463132a5eec6e19eadc099
18372738 <133884450+18372738@users.noreply.github.com>
2023-12-20 03:04:21 +0300

    publish photo to Telegram channel

--- publishes_all_photos_telegram.py
+++ publishes_all_photos_telegram.py
@@ -9,4 +9,5 @@ env = Env()
 env.read_env()
 tg_token = env.str('TOKEN_TELEGRAM')
 bot = telegram.Bot(token=tg_token)
-bot.send_message(chat_id=env.str('CHAT_ID_TELEGRAM'), text="Привет Бот")
+path = "images/nasa_apod_9.jpg"
+bot.send_photo(chat_id=env.str('CHAT_ID_TELEGRAM'), photo=open(path, 'rb'))
