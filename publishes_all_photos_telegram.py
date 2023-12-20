0a9b28df66d0442fd078599e3e6bd783b7e5bafd
18372738 <133884450+18372738@users.noreply.github.com>
2023-12-20 03:05:39 +0300

    publishes all photos

--- publishes_all_photos_telegram.py
+++ publishes_all_photos_telegram.py
@@ -1,13 +1,29 @@
+import argparse
 import os
+import random
+import time
 
 import telegram
 from environs import Env
 
 
+def main():
+    env = Env()
+    env.read_env()
+    tg_token = env.str('TOKEN_TELEGRAM')
+    chat_id = env.str('CHAT_ID_TELEGRAM')
+    bot = telegram.Bot(token=tg_token)
+    parser = argparse.ArgumentParser(description="Программа высылает фото в чат с указанной задержкой")
+    parser.add_argument("--delay", help="Задержка", default=14400)
+    args = parser.parse_args()
+    posting_time = args.delay
+    while True:
+        for folder, list, files in os.walk("images"):
+            random.shuffle(files)
+            for file in files:
+                path = os.path.join(folder, file)
+                bot.send_photo(chat_id=chat_id, photo=open(path, "rb"))
+                time.sleep(posting_time)
 
-env = Env()
-env.read_env()
-tg_token = env.str('TOKEN_TELEGRAM')
-bot = telegram.Bot(token=tg_token)
-path = "images/nasa_apod_9.jpg"
-bot.send_photo(chat_id=env.str('CHAT_ID_TELEGRAM'), photo=open(path, 'rb'))
+if __name__ == "__main__":
+    main()
